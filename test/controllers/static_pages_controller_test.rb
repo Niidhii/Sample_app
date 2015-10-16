52c33362105c12de921c931728cfcde97063727b
Niidhii <nidhi.g.solanki@gmail.com>
2015-10-16 11:59:26 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,10 +16,18 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
+  
  test "should get about" do
    get :about
    assert_response :success
    assert_select "title", "About | #{@base_title}"
-   
  end
+  test "should get contatc" do 
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}" 
+  end
+  
 end
+ 
+ 
